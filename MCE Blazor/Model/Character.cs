a93c1fb4aca9520e58210e09ccaf18249cc48616
Simon Bingel <bingel.simon@gmail.com>
2021-06-20 13:56:43 +0200

    Import Page WIP

--- MCE Blazor/Model/Character.cs
+++ MCE Blazor/Model/Character.cs
@@ -7,38 +7,42 @@ namespace MCE.Model
     /// </summary>
     public class Character
     {
-        public int         Id           { get; set; }
-        public string      Name         { get; set; }
-        public Race        Race         { get; set; }
-        public Class       Class        { get; set; }
-        public int         Age          { get; set; }
-        public string      Size         { get; set; }
-        public string      Weight       { get; set; }
-        public string      Build        { get; set; }
-        public string      Handedness   { get; set; }
-        public SocialClass SocialClass  { get; set; }
-        public Land        Origin       { get; set; }
-        public string      Religion     { get; set; }
-        public int         SmilingFate  { get; set; }
-        public int         DivineMercy  { get; set; }
-        public string      Story        { get; set; }
-        public int         Strength     { get; set; }
-        public int         Dexterity    { get; set; }
-        public int         Agility      { get; set; }
-        public int         Constitution { get; set; }
-        public int         Intelligence { get; set; }
-        public int         MagicTalent  { get; set; }
-        public int         Health       { get; set; }
-        public int         Stamina      { get; set; }
-        public int         Appearance   { get; set; }
-        public int         Charisma     { get; set; }
-        public int         Defense      { get; set; }
-        public int         Resistance   { get; set; }
-        public int         Movement     { get; set; }
-        public int         Xp           { get; set; }
-        public int         Level        { get; set; }
-        public string      ArmorClass   { get; set; }
-        public int         OpenXp       { get; set; }
+        public int         Id            { get; set; }
+        public string      Name          { get; set; }
+        public int         RaceId        { get; set; }
+        public Race        Race          { get; set; }
+        public int         ClassId       { get; set; }
+        public Class       Class         { get; set; }
+        public int         Age           { get; set; }
+        public string      Size          { get; set; }
+        public string      Weight        { get; set; }
+        public string      Build         { get; set; }
+        public string      Handedness    { get; set; }
+        public int         SocialClassId { get; set; }
+        public SocialClass SocialClass   { get; set; }
+        public int         OriginId      { get; set; }
+        public Land        Origin        { get; set; }
+        public string      Religion      { get; set; }
+        public int         SmilingFate   { get; set; }
+        public int         DivineMercy   { get; set; }
+        public string      Story         { get; set; }
+        public int         Strength      { get; set; }
+        public int         Dexterity     { get; set; }
+        public int         Agility       { get; set; }
+        public int         Constitution  { get; set; }
+        public int         Intelligence  { get; set; }
+        public int         MagicTalent   { get; set; }
+        public int         Health        { get; set; }
+        public int         Stamina       { get; set; }
+        public int         Appearance    { get; set; }
+        public int         Charisma      { get; set; }
+        public int         Defense       { get; set; }
+        public int         Resistance    { get; set; }
+        public int         Movement      { get; set; }
+        public int         Xp            { get; set; }
+        public int         Level         { get; set; } = 1;
+        public string      ArmorClass    { get; set; }
+        public int         OpenXp        { get; set; }
 
         public List<CharacterHasSpell>  CharacterHasSpells  { get; set; } = new List<CharacterHasSpell>();
         public List<CharacterHasSkill>  CharacterHasSkills  { get; set; } = new List<CharacterHasSkill>();
